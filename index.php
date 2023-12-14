587774a5a91c041154fbe43e3e4d951acc27e252
LeoGidev <leogimenez.dev@gmail.com>
2023-12-14 14:33:29 -0300

    se pone un else en donde manejamos el envio del ip al php que hhace el ping (ping.php) y tenemos en cuenta el metodo para esto dependiendo del buscador, aunque en la actualidad internet explorer no se usa

--- index.php
+++ index.php
@@ -26,6 +26,27 @@ function showUser(str) {
         document.getElementById("txtHint").innerHTML = "";
         return;
     }
+    else { 
+        if (window.XMLHttpRequest) {
+            // para IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            // para IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function() {
+			
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("txtHint").innerHTML = this.responseText;
+				
+				setInterval(ocultar(),3000);
+				
+            }
+        };
+        xmlhttp.open("GET","ping.php?q="+str,true);
+		
+        xmlhttp.send();
+    }
 
 
  }
