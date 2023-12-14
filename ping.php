96ce20fb2596b8167c701e710f2df08ffa36271a
LeoGidev <leogimenez.dev@gmail.com>
2023-12-14 15:00:34 -0300

    si en la respuesta aparacen los valores típicos de un ping sin conexión se devuelve la expresión Ip Down

--- ping.php
+++ ping.php
@@ -2,5 +2,10 @@
 $q = $_GET['q'];
 $comando = "ping ".$q." -n 2 ";
 $output = shell_exec($comando);
+if (strpos($output, "recibidos = 0")|| strpos($output, "vuelva a intentarlo.")||strpos($output, "TTL expirado")||strpos($output, "Host de destino inaccesible") ) {
+    echo "<ul class='list-group list-group-horizontal'><li class='list-group-item list-group-item-danger'>";
+	echo $q;
+	echo " Down</li></ul>";
+}
 
 ?>
